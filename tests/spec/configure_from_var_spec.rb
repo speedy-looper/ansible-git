dea61a0e5d20d20a8f1f997450c967a07838bba3
Maxime Loliée <maxime@siliadev.com>
2015-06-29 01:33:10 +0200

    fix: Travis build

--- tests/spec/configure_from_var_spec.rb
+++ tests/spec/configure_from_var_spec.rb
@@ -4,5 +4,5 @@ set :backend, :exec
 
 describe file('/home/mtester/.gitconfig') do
   it { should be_file }
-  its(:content) { should_not contain /email = thedude@tester\.com/ }
+  its(:content) { should contain 'email = thedude@tester.com' }
 end
